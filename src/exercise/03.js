869e33ec05fc7bd823816e397aab9a6725f1d742
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2023-08-05 11:40:09 -0700

    Update .prettierrc

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -1,34 +1,34 @@
 // Lifting state
 // http://localhost:3000/isolated/exercise/03.js
 
-import * as React from 'react'
+import * as React from 'react';
 
 function Name() {
   const [name, setName] = React.useState('');
-  
+
   return (
     <div>
       <label htmlFor="name">Name: </label>
-      <input id="name" value={name} onChange={event => setName(event.target.value)} />
+      <input
+        id="name"
+        value={name}
+        onChange={event => setName(event.target.value)}
+      />
     </div>
-  )
+  );
 }
 
-function FavoriteAnimal({ animal, onAnimalChange }) {
+function FavoriteAnimal({animal, onAnimalChange}) {
   return (
     <div>
       <label htmlFor="animal">Favorite Animal: </label>
-      <input
-        id="animal"
-        value={animal}
-        onChange={onAnimalChange}
-      />
+      <input id="animal" value={animal} onChange={onAnimalChange} />
     </div>
-  )
+  );
 }
 
 function Display({animal}) {
-  return <div>{`Your favorite animal is: ${animal}!`}</div>
+  return <div>{`Your favorite animal is: ${animal}!`}</div>;
 }
 
 function App() {
@@ -37,10 +37,13 @@ function App() {
   return (
     <form>
       <Name />
-      <FavoriteAnimal animal={animal} onAnimalChange={event => setAnimal(event.target.value)}/>
-      <Display animal={animal}/>
+      <FavoriteAnimal
+        animal={animal}
+        onAnimalChange={event => setAnimal(event.target.value)}
+      />
+      <Display animal={animal} />
     </form>
-  )
+  );
 }
 
-export default App
+export default App;
