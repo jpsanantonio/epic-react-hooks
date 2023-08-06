1257f8279d6c58ae3c81d20173865ad738fc9398
Jayvic San Antonio <codes@jayvicsanantonio.dev>
2023-08-05 18:28:55 -0700

    useEffect: HTTP requests - Reset the ErrorBoundary

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -41,11 +41,12 @@ function PokemonInfo({ pokemonName }) {
   }
 }
 
-function ErrorFallback({ error }) {
+function ErrorFallback({ error, resetErrorBoundary }) {
   return (
     <div role="alert">
       There was an error:{' '}
       <pre style={{ whiteSpace: 'normal' }}>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
     </div>
   );
 }
@@ -57,12 +58,16 @@ function App() {
     setPokemonName(newPokemonName);
   }
 
+  function handleReset() {
+    setPokemonName('');
+  }
+
   return (
     <div className="pokemon-info-app">
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <ErrorBoundary key={pokemonName} FallbackComponent={ErrorFallback}>
+        <ErrorBoundary FallbackComponent={ErrorFallback} onReset={handleReset}>
           <PokemonInfo pokemonName={pokemonName} />
         </ErrorBoundary>
       </div>
